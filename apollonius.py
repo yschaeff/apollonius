df42e310558439833d7061ca3049619a1b1ee0c5
Yuri Schaeffer <yuri@nlnetlabs.nl>
2017-04-04 22:41:24 +0200

    fix bugs, implement for lines

--- apollonius.py
+++ apollonius.py
@@ -1,28 +1,68 @@
+#!/usr/bin/env python3
 import math
 import matplotlib.pyplot as plt
+import matplotlib
 from numpy import array, dot
 from numpy.linalg import norm
 
-C1 = array([0,0])
-r1 = 2.0
-C2 = array([10,0])
-r2 = 8.0
-C3 = array([1,8])
-r3 = 1.0
+class Obj:
+    pass
 
-P = (C1+C2+C3)/3
-r = (r1+r2+r3)/3
+class Circle(Obj):
+    def __init__(s, x, y, r):
+        s.p = array([x, y])
+        s.r = r
 
-def pull(C, r, P):
-    """Find the force circle C with radius r applies to point P """
-    f = C - P
-    n = norm(f)
-    # if n==0 the force has no direction!
-    if n == 0: return array([0,0])
-    if n<r:
-        return - f * (1 - r/n)
-    else:
-        return f * (1 - r/n)
+    def force(s, p):
+        """Find the force circle C with radius r applies to point P """
+        f = s.p - p
+        n = norm(f)
+        # if n==0 the force has no direction!
+        if n == 0: return array([0,0])
+        if n < s.r:
+            print("err");
+            return -f * (1 - s.r/n)
+            return -f * (s.r/n)
+        else:
+            return f * (1 - s.r/n)
+
+    def plt(s, ax):
+        ax.add_patch(plt.Circle(s.p, s.r))
+
+class Line(Obj):
+    def __init__(s, a, c, n):
+        s.a = a
+        s.c = c
+        #s.x = array([x1, x2])
+        #s.y = array([y1, y2])
+        s.n = n
+
+    def force(s, p):
+        """Find the force circle C with radius r applies to point P """
+        x = (p[0] + s.a*p[1] - s.a*s.c) / (s.a**2 + 1)
+        y = (s.a*(p[0]+s.a*p[1]) + s.c) / (s.a**2 + 1)
+        d = array([x, y])
+
+        return d - p
+
+        y = s.b * p[0] + s.c
+        x = (p[1] - s.c)/s.b
+        dx = p[0] - x
+        dy = p[1] - y
+        
+        d = y - p[n]
+        f = s.p - p
+        n = norm(f)
+        # if n==0 the force has no direction!
+        if n == 0: return array([0,0])
+        if n < s.r:
+            return - f * (1 - s.r/n)
+        else:
+            return f * (1 - s.r/n)
+
+    def plt(s, ax):
+        xmin, xmax = ax.get_xbound()
+        ax.add_line(matplotlib.lines.Line2D([xmin,xmax], [s.a*xmin+s.c, s.a*xmax+s.c], linewidth=2, color='blue'))
 
 def ev(v, n):
     """give the error vector given vector v and len l"""
@@ -33,34 +73,44 @@ def error_vectors(f1, f2, f3):
     n = (norm(f1) + norm(f2) + norm(f3))/3
     return ev(f1, n), ev(f2, n), ev(f3, n)
 
+def start_pos(C):
+    return array(C.p), C.r
+
+C1 = Circle( 0, 0, 2.0)
+C2 = Circle(10, 0, 8.0)
+C3 = Circle( 1, 8, 1.0)
+L = Line(1, 0, 0)
+P, r = start_pos(C2)
+
 fig = plt.figure(1)
 plt.axis([-10, 20, -10, 10])
 ax = fig.add_subplot(1,1,1)
-c1 = plt.Circle((C1[0], C1[1]), r1)
-c2 = plt.Circle((C2[0], C2[1]), r2)
-c3 = plt.Circle((C3[0], C3[1]), r3)
-ax.add_patch(c1)
-ax.add_patch(c2)
-ax.add_patch(c3)
+#L.plt(ax)
+C1.plt(ax)
+C2.plt(ax)
+C3.plt(ax)
 
 p = plt.Circle(P, r, fill=False, color="%f"%(0/26.0))
 ax.add_patch(p)
 
 for i in range(1000):
-    f1 = pull(C1, r1, P)
-    f2 = pull(C2, r2, P)
-    f3 = pull(C3, r3, P)
+    f1 = C1.force(P)
+    f2 = C2.force(P)
+    f3 = C3.force(P)
+    #f3 = L.force(P)
 
     ef1, ef2, ef3 = error_vectors(f1, f2, f3)
     P = P + (ef1 + ef2 + ef3)/3
     r = (norm(f1) + norm(f2) + norm(f3))/3
     e = (norm(ef1) + norm(ef2) + norm(ef3))
-    #print i, norm(ef1), norm(ef2), norm(ef3), P, e
-    p = plt.Circle(P, r, fill=False, color=(i/25.0, 0, 0))
+    print(i, norm(ef1), norm(ef2), norm(ef3), P, e)
+    p = plt.Circle(P, r, fill=False)
     ax.add_patch(p)
-    if e < 0.01: break
+    if e < 0.001: break
     continue
 
-print P, r
+print (P, r)
+p = plt.Circle(P, r, fill=False, color=".6")
+ax.add_patch(p)
 
 plt.show()
