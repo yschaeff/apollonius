7e7b6253ad4bbf6dba02e2e76b65b835bc6b55f1
Yuri Schaeffer <yuri@nlnetlabs.nl>
2017-12-15 22:29:21 +0100

    Harder, better, faster, stronger

--- 2/ap.py
+++ 2/ap.py
@@ -1,6 +1,28 @@
 from numpy import array, dot, matrix, ravel, arange
 from numpy.linalg import norm
 
+class Circle:
+    def __init__(self, p, r):
+        self.p = p
+        self.r = r
+
+    def __lt__(self, c):
+        return self.r < c.r
+
+    def update_radius(self, C):
+        for c in C:
+            if self.r and self.r < 0: break
+            #self.r = norm(self.p - c.p) - c.r
+            r = norm(self.p - c.p) - c.r
+            if  self.r == None or r < self.r:
+                self.r = r
+
+    def export(self):
+        print("translate([%f, %f, %f]) sphere(r=%f);"%(self.p[0], self.p[1], 0, self.r+EPSILON/2))
+
+    def __repr__(self):
+        return "({}, {})".format(self.p, self.r)
+
 def find_3rth(v1, v2, epsilon):
     a = v2-v1
     l = norm(a)
@@ -19,38 +41,18 @@ def find_inscribed(A, B, C):
     UU = array([x, y])
     U = UU + A
     r = norm(UU)
-    return U, r
+    return Circle(U, r)
 
 def boundingbox(vertices):
     BB = [[v[i] for v in vertices] for i in range(DIM)]
     BB = [(min(c), max(c)) for c in BB]
     return BB
 
-def inside(p, U):
-    return norm(U[0]-p) <= U[1]
-
-def inside_any(p, Us):
-    return any(map(lambda U: inside(p, U), Us))
-
-def distance(p, U):
-    return norm(U[0]-p) - U[1]
-
-def min_distance(p, Us):
-    return min(map(lambda U: distance(p, U), Us))
-
-def export(U):
-    c = U[0]
-    x, y = c
-    r = U[1]
-    print("translate([%f, %f, %f]) sphere(r=%f);"%(x, y, 0, r+EPSILON/2))
-
-EPSILON = .1
+EPSILON = .01
 DIM = 2
 vertices = [array([0,0]), array([10,0]), array([0, 10])]
 faces = [(0, 1), (1, 2), (2, 0)] #right hand rule
 
-BB = boundingbox(vertices)
-obj = []
 initial_spheres = []
 
 for face in faces:
@@ -61,21 +63,23 @@ for face in faces:
     initial_spheres.append(C)
 
 cand = []
+BB = boundingbox(vertices)
 for x in arange(BB[0][0], BB[0][1], EPSILON):
     for y in arange(BB[1][0], BB[1][1], EPSILON):
-        cand.append(array([x, y]))
+        cand.append(Circle(array([x, y]), None))
 
 print("$fs=.1;")
 print("$fa=1;")
 
-cand = list(filter(lambda p: not inside_any(p, initial_spheres), cand))
-obj.extend(initial_spheres)
-while cand:
-    d = list(map(lambda p: (p, min_distance(p, obj)), cand))
-    new_obj = max(d, key=lambda x: x[1])
-    if new_obj[1] < EPSILON: break
-    ### optionally wiggle winner until touching 3 winners within e
-    cand = list(filter(lambda p: not inside_any(p, [new_obj]), cand))
-    obj.append(new_obj)
-    export(new_obj)
+[c.update_radius(initial_spheres) for c in cand]
+cand.sort(reverse=True)
+while len(cand) > 0:
+    w = cand.pop(0)
+    if w.r < EPSILON: break
+    for i, c in enumerate(cand):
+        if c.r <= 0: break
+        c.update_radius([w])
+    cand = cand[:i]
+    cand.sort(reverse=True)
+    w.export()
 
