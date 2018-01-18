cfb64f4ae4d23bf64db3742f06508f9f8257282b
Yuri Schaeffer <yuri@nlnetlabs.nl>
2018-01-18 09:02:36 +0100

    multipass implementation

--- reduce.py
+++ reduce.py
@@ -3,174 +3,67 @@ import numpy as np
 from collections import defaultdict
 import bisect
 
-def normal(face, vs):
-    ui, vi, wi = face
-    u = vs[ui]
-    v = vs[ui]
-    w = vs[ui]
-    n = np.cross(u-v, w-v)
-    return n/np.linalg.norm(n)
 
-def get_normals(mesh, normals, fis):
-    for fi in fis:
-        if fi in normals:
-            yield normals[fi]
-        else:
-            yield mesh.units[fi]
+#for each vertex find all neighbours
+#for each vertex find cost (based on neighbours)
+#sort by cost
+#find cutoff cost
+#For each choosen ones collapse all unchoosen ones
+    #and cache that
 
-def insort(array, item, key):
-    ci = key(item)
-    s = 0
-    e = len(array)
-    p = (s+e)//2
-    while s < e:
-        cp = key(array[p])
-        if ci > cp:
-            s = p+1
-        elif ci < cp:
-            e = p
-        else:
-            break
-        p = (s+e)//2
-    array.insert(p, item)
+def weight(uH, VH):
+    u = np.array(uH)
+    V = [np.array(v) for v in VH]
+    return max(abs(np.sum([v-u for v in V], 0)))
+
+def toStrong(uH, collapses):
+    while uH in collapses:
+        uH = collapses[uH]
+    return uH
 
 def reduce(mesh, N):
-    ## Index all vertices and faces
-    v2vi = {} # numpy arrays to index in vs
-    vs = []   # numpy arrays
-    VI = 0    # next index number
-    vi2fis = defaultdict(set)  # faces for vertex index
-    fi2vis = defaultdict(list)  # vertices for face index
-    normals = {} ## overlay
-    for fi, face in enumerate(mesh.vectors):
-        vis = fi2vis[fi] #vertices for this face
-        for v in face:
-            if tuple(v) not in v2vi:
-                v2vi[tuple(v)] = VI
-                vs.append(v)
-                vi = VI
-                VI += 1
-            else:
-                vi = v2vi[tuple(v)]
-            vis.append(vi)
-            vi2fis[vi].add(fi)
+    neighbours = defaultdict(set)
+    for face in mesh.vectors:
+        for ui, u in enumerate(face):
+            v = face[(ui+1)%3]
+            vH = tuple(v)
+            uH = tuple(u)
+            neighbours[vH].add(uH)
+            neighbours[uH].add(vH)
+    weights = dict()
+    for uH, VH in neighbours.items():
+        weights[uH] = weight(uH, VH)
+    vertices = sorted(weights.items(), key = lambda x: x[1], reverse = True)
+    n = len(vertices) - 1
+    if N < n: n = N
+    u, cutoff_weight = vertices[n] ## first of the weak points
+    #print(u, cutoff_weight)
+    Sstrong = set([v[0] for v in vertices[:n]])
+    print(len(vertices), len(Sstrong), n)
+    collapses = dict()
+    done = False
+    while not done:
+        done = True
+        for vH in Sstrong:
+            UH = neighbours[vH]
+            UH.difference_update(Sstrong)
+            N = set()
+            for uH in UH:
+                if uH in collapses: continue
+                done = False
+                collapses[uH] = vH
+                N.update(neighbours[uH])
+            N.difference_update(set([vH]))
+            UH.update(N)
 
-    edges = {}
-    for fi, vis in fi2vis.items():
-        #construct edges
-        for i, v1 in enumerate(vis):
-            v2 = vis[(i+1)%3]
-            def calc_cost(v1, v2):
-                u = vs[v1]
-                v = vs[v2]
-                v1fis = vi2fis[v1]
-                v2fis = vi2fis[v2]
-                v12fis = v1fis.intersection(v2fis)
-                Tu = list(get_normals(mesh, normals, v1fis))
-                Tuv = list(get_normals(mesh, normals, v12fis))
-                #return (1-min([min([np.dot(f, n) for n in Tuv]) for f in Tu]))/2
-                return np.linalg.norm(u-v) * \
-                    (1-min([max([np.dot(f, n) for n in Tuv]) for f in Tu]))/2
-
-            Fedge = (v1, v2)
-            if Fedge not in edges:
-                edges[Fedge] = calc_cost(v1, v2)
-            Redge = (v2, v1)
-            if Redge not in edges:
-                edges[Redge] = calc_cost(v2, v1)
-
-    sorted_edges = list(edges.keys())
-    sorted_edges.sort(key = lambda x: edges[x])
-
-    deleted_edges = set()
-    deleted_vertices = set()
-    deleted_faces = set()
-    while len(fi2vis) - len(deleted_faces) > N and sorted_edges:
-        #print(len(fi2vis), len(deleted_faces), N, l)
-        #sorted_edges.sort(key = lambda x: edges[x]) ## TMP
-        edge = sorted_edges.pop(0)
-        #print("collapsing", edge, vs[edge[0]], vs[edge[1]])
-        if edge in deleted_edges:
-            #print("nvm")
-            continue
-        ui, vi = edge
-        assert(ui != vi)
-        if ui in deleted_vertices:
-            #print("u deleted, skip, todo add new edge")
-            continue
-        if vi in deleted_vertices:
-            #print("v deleted, skip, todo add new edge")
-            continue
+    ## now write faces, translate all vertices if any two match ->delete
+    data = np.zeros(50000, dtype = stl.mesh.Mesh.dtype)
+    for i, face in enumerate(mesh.vectors):
+        UH = [toStrong(tuple(u), collapses) for u in face]
+        if UH[0] == UH[1] or UH[0] == UH[2] or UH[1] == UH[2]:
+            pass
         else:
-            deleted_vertices.add(ui)
-        deleted_edges.add((vi, ui)) # reverse
-        deleted_edges.add((ui, vi)) # forward
-        u_fis = vi2fis[ui]
-        v_fis = vi2fis[vi]
-        uv_fis = u_fis.intersection(v_fis)
-        
-        ## remove collapsed faces from any vertices
-        for fi in uv_fis:
-            vis = fi2vis[fi]
-            for v in vis:
-                vi2fis[v].remove(fi)
-        deleted_faces.update(uv_fis)
-
-        edges_to_update = []
-        ## all leftover faces in add to v AND update face[u]=v
-        v_fis.update(u_fis)
-        for fi in u_fis:
-            face = fi2vis[fi]
-            for i, v in enumerate(face):
-                if v==ui: face[i] = vi
-            normals[fi] = normal(face, vs)
-            for i, v in enumerate(face):
-                v2 = face[(i+1)%3]
-                edges_to_update.append((v, v2))
-                edges_to_update.append((v2, v))
-
-
-        #TODO: find all edges with ui. update to vi. recalc cost
-        if False:
-            for edge in sorted_edges:
-                if any([v==ui for v in edge]):
-                    edges_to_update.append(edge)
-            #print(len(edges_to_update))
-            #print(edges_to_update)
-            for (v1, v2) in edges_to_update:
-                #print((v1, v2))
-                if (v2, v1) in deleted_edges:
-                    continue
-                #print("whelp", v1, v2, ui, vi, vi2fis[v1], vi2fis[v2])
-                if v1 == ui:
-                    deleted_edges.add((v1, v2))
-                    deleted_edges.add((v2, v1))
-                    v1 = vi
-                elif v2 == ui:
-                    deleted_edges.add((v1, v2))
-                    deleted_edges.add((v2, v1))
-                    v2 = vi
-                else:
-                    print("just to recalc")
-                    pass
-
-                edge = v1, v2
-                #print("new: ", edge)
-                cost = calc_cost(v1, v2)
-                edges[edge] = cost
-                #sorted_edges.insert(0, edge)
-                insort(sorted_edges, edge, lambda x: edges[x])
-
-
-    print(len(deleted_faces), len(sorted_edges))
-    data = np.zeros(len(fi2vis) - len(deleted_faces), dtype = stl.mesh.Mesh.dtype)
-    i = 0
-    for fi, vis in fi2vis.items():
-        if fi in deleted_faces: continue
-        V = [vs[vi] for vi in vis]
-        data['vectors'][i] = np.stack(V)
-        #print(data['vectors'][i],  np.stack(V))
-        i += 1
+            data['vectors'][i] = np.stack([np.array(uH) for uH in UH])
     reduced_mesh = stl.mesh.Mesh(data)
     return reduced_mesh
 
